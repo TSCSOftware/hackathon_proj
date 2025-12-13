d4c9d293605feaa49266f02daa5c4c8ecef82555
Adnaan-codes <adnaansaleem514@gmail.com>
2025-12-13 18:58:35 +0530

    8.4

--- lib/Web/web dashboard.dart
+++ lib/Web/web dashboard.dart
@@ -99,13 +99,14 @@ class _WebDashboardPageState extends State<WebDashboardPage> {
           Expanded(
             child: OSMFlutter(
   controller: MapController(
-    initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
+    initPosition: GeoPoint(latitude: 6.7056, longitude: 80.3847),
     areaLimit: const BoundingBox(
       east: 10.4922941,
       north: 47.8084648,
       south: 45.817995,
       west: 5.9559113,
     ),
+     
   ),
   osmOption: OSMOption(
     userTrackingOption: const UserTrackingOption(
@@ -113,7 +114,7 @@ class _WebDashboardPageState extends State<WebDashboardPage> {
       unFollowUser: false,
     ),
     zoomOption: const ZoomOption(
-      initZoom: 8,
+      initZoom: 12,
       minZoomLevel: 3,
       maxZoomLevel: 19,
       stepZoom: 1.0,
